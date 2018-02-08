96a26ab04a744b9ddffdf74e2dd1a32080e6e3c6
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-02-08 21:56:47 +0400

    Start create project

---
+++ dev/js/chinastories.js
@@ -0,0 +1,14 @@
+(function($) {
+	var CHINASTORIES = (function() {
+
+		var $sel = {};
+		$sel.window = $(window);
+		$sel.html = $("html");
+		$sel.body = $("body", $sel.html);
+
+		return {
+
+		};
+
+	})();
+})(jQuery);
